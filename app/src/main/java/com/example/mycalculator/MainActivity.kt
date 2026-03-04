bfcd9fc3d26af8fc896779c3acb68b8b500c1115
fukkut <Sviatoslav@DESKTOP-E0FH1K4>
2026-03-04 21:17:07 +0200

    Додано змінні та підключено TextView

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.mycalculator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var firstNumber = ""
+    private var secondNumber = ""
+    private var operation = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val tvResult = findViewById<TextView>(R.id.tvResult)
     }
 }
